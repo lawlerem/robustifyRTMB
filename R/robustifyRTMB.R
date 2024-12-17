4c353a46ce8aa5f5ed385797e1b920d10a7fab4c
Ethan Lawler <lawlerem@gmail.com>
2024-12-17 11:53:57 -0400

    Allow vectorized evaluation of robustify and robust_weight

--- R/robustifyRTMB.R
+++ R/robustifyRTMB.R
@@ -2,87 +2,99 @@
 #' 
 #' Compute the robustified likelihood value
 #' 
-#' @param x A likelihood value
+#' @param x A vector of log-likelihood values
 #' @param robustness A positive number. Larger numbers downweight components more.
 #' @param robust_function Either "id" for the identity (non-robust), "ll" for log-logistic, or "ssh" for smooth semi-huber.
 #' 
-#' @return A numeric value giving the robustified likelihood component.
+#' @return The robustified log-likelihood values.
 #' 
 #' @export
 robustify<- function(x, robustness, robust_function = "id") NULL
 
-#' Compute the robust weight of a likelihood component
+#' Compute the robust weight of a log-likelihood value
 #' 
-#' @return A numeric value giving the robust weight.
+#' @return A vector of robust weights.
 #' 
-#' @describeIn robustify Compute the robust weight of a likelihood component
+#' @describeIn robustify Compute the robust weight of log-likelihood values
 #' 
 #' @export
 robust_weight<- function(x, robustness, robust_function = "id") NULL
 
 .onLoad<- function(libname, pkgname) {
-  .orig_TapeConfig<- RTMB::TapeConfig()
-  RTMB::TapeConfig(comparison = "tape")
+    .orig_TapeConfig<- RTMB::TapeConfig()
+    RTMB::TapeConfig(comparison = "tape")
 
-  .ll<- RTMB::MakeTape(
-    function(y) {
-      x<- y[[1]]
-      c<- 1 / (y[[2]] + 0.002) # give upper bound to c to avoid overflow later
-      ans<- log(1 + exp(x + c)) - log(1 + exp(c))
-      ans<- (y[[2]] == 0) * x + (y[[2]] > 0) * ans
-      return(ans)
-    },
-    c(0, 1)
-  )$atomic()
-  .ll_jac<- .ll$jacfun()
+    .ll<- RTMB::MakeTape(
+        function(y) {
+            x<- y[[1]]
+            c<- 1 / (y[[2]] + 0.002) # give upper bound to c to avoid overflow later
+            ans<- log(1 + exp(x + c)) - log(1 + exp(c))
+            ans<- (y[[2]] == 0) * x + (y[[2]] > 0) * ans
+            return(ans)
+        },
+        c(0, 1)
+    )$atomic()
+    .ll_jac<- .ll$jacfun()
 
-  .ssh<- RTMB::MakeTape(
-    function(y) {
-      x<- y[[1]]
-      c<- 1 / (y[[2]] + .Machine$double.eps)
-      u<- (x + c) / c
-      ans<- (x >= -c) * (x) + 
-        (x < -c) * (c * log(u + sqrt(1 + u^2)) - c)
-      ans<- (y[[2]] == 0) * x + (y[[2]] > 0) * ans
-      return(ans)
-    },
-    c(0, 1)
-  )$atomic()
-  .ssh_jac<- .ssh$jacfun()
+    .ssh<- RTMB::MakeTape(
+        function(y) {
+            x<- y[[1]]
+            c<- 1 / (y[[2]] + .Machine$double.eps)
+            u<- (x + c) / c
+            ans<- (x >= -c) * (x) + 
+                (x < -c) * (c * log(u + sqrt(1 + u^2)) - c)
+            ans<- (y[[2]] == 0) * x + (y[[2]] > 0) * ans
+            return(ans)
+        },
+        c(0, 1)
+    )$atomic()
+    .ssh_jac<- .ssh$jacfun()
 
-  do.call(RTMB::TapeConfig, as.list(.orig_TapeConfig))
+    do.call(RTMB::TapeConfig, as.list(.orig_TapeConfig))
 
-  robustify<- function(x, robustness, robust_function = "id") {
-    ans<- switch(
-      robust_function,
-      "id" = x,
-      "ll" = .ll(c(x, robustness)),
-      "ssh" = .ssh(c(x, robustness)),
-      default = x
+    robustify<- function(x, robustness, robust_function = "id") {
+        ans<- RTMB::sapply(
+            x,
+            function(xx) {
+                yy<- switch(
+                    robust_function,
+                    "id" = xx,
+                    "ll" = .ll(c(xx, robustness)),
+                    "ssh" = .ssh(c(xx, robustness)),
+                    default = xx
+                )
+                return(yy)
+            }
+        )
+        return(ans)
+    }
+    assign(
+        "robustify",
+        robustify,
+        parent.env(environment())
     )
-    return(ans)
-  }
-  assign(
-    "robustify",
-    robustify,
-    parent.env(environment())
-  )
 
-  robust_weight<- function(x, robustness, robust_function = "id") {
-    ans<- switch(
-      robust_function,
-      "id" = 1,
-      "ll" = .ll_jac(c(x, robustness))[1],
-      "ssh" = .ssh_jac(c(x, robustness))[1],
-      default = 1
+    robust_weight<- function(x, robustness, robust_function = "id") {
+        ans<- RTMB::sapply(
+            x,
+            function(xx) {
+                yy<- switch(
+                    robust_function,
+                    "id" = 1,
+                    "ll" = .ll_jac(c(xx, robustness))[1],
+                    "ssh" = .ssh_jac(c(xx, robustness))[1],
+                    default = 1
+                )
+                return(yy)
+            }
+        )
+        return(ans)
+    }
+    assign(
+        "robust_weight",
+        robust_weight,
+        parent.env(environment())
     )
-    return(ans)
-  }
-  assign(
-    "robust_weight",
-    robust_weight,
-    parent.env(environment())
-  )
 
-  invisible()
+    invisible()
 }
