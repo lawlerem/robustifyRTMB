fdbb2ee0f96baa3010f300940e08455d82ffa741
Ethan Lawler <lawlerem@gmail.com>
2024-12-25 00:39:47 -0400

    Linted code, updated documentation, and improved tracing messages

--- R/robust_optimization.R
+++ R/robust_optimization.R
@@ -1,17 +1,34 @@
 #' Fit a random effects model with a robustified likelihood
 #' 
-#' @param func See RTMB::MakeADFun
-#' @param parameters See RTMB::MakeADFun
-#' @param random See RTMB::MakeADFun
-#' @param smooth A character naming the random effects that should be smoothed
-#' @param nodes A matrix giving the locations of the spline nodes
-#' @param robust_schedule The sequence of robustness values to iterate through
-#' @param map See RTMB::MakeADFun
-#' @param bandwidth The bandwidth to use when smoothing the spline between iterations
-#' @param inner.trace Should the inner tracing (nlminb) be printed?
-#' @param outer.trace Should the outer tracing (iterative optimization) be printed?
+#' @param func 
+#'     See RTMB::MakeADFun
+#' @param parameters 
+#'     See RTMB::MakeADFun
+#' @param random 
+#'     See RTMB::MakeADFun
+#' @param smooth 
+#'     A character naming the random effects that should be smoothed. 
+#' @param nodes 
+#'     A matrix giving the locations of the spline nodes
+#' @param robust_schedule 
+#'     The sequence of robustness values to iterate through
+#' @param map 
+#'     See RTMB::MakeADFun
+#' @param bandwidth 
+#'     The bandwidth to use when smoothing the spline between iterations
+#' @param silent
+#'     Should the optimization tracing be suppressed?
 #' 
-#' @return A list containing the MakeADFun, the nlminb, and sdreport outputs
+#' @return 
+#'     A list containing:
+#'   * par 
+#'       The estimated parameter list
+#'   * obj 
+#'       The objective function from RTMB::MakeADFun
+#'   * opt 
+#'       The optimizer output from nlminb
+#'   * sdr 
+#'       The standard errors from RTMB::sdreport
 #' 
 #' @export
 robustly_optimize<- function(
@@ -25,49 +42,42 @@ robustly_optimize<- function(
         bandwidth = 1e-6,
         silent = TRUE
     ) {
-    weight_fun<- Vectorize(
-        function(d, bandwidth) exp( -(d / bandwidth) ),
-        "d"
-    )
-    d<- stats::dist(
-        nodes,
-        diag = TRUE,
-        upper = TRUE
-    )
-    d<- as.matrix(d)
+    weight_fun<- (function(d, bandwidth) exp( -(d / bandwidth) )) |>
+        Vectorize("d")
+    d<- nodes |> stats::dist(diag = TRUE, upper = TRUE) |> as.matrix()
     within<- d <= (2 * bandwidth)
     weights<- 0 * d
-    weights[within]<- weight_fun(d[within], bandwidth)
-    weights<- sweep(
-        weights,
-        MARGIN = 1,
-        STATS = rowSums(weights),
-        FUN = "/"
-    )
+    weights[within]<- d[within] |> weight_fun(bandwidth)
+    weights<- weights |> 
+        sweep(
+            MARGIN = 1,
+            STATS = weights |> rowSums(),
+            FUN = "/"
+        )
     tighten<- function(node_values) weights %*% cbind(node_values)
 
     if( "robustness" %in% names(parameters) ) {
-        warning("'robustness' is a reserved parameter for robustly_optimize. Replacing it from the supplied parameter list.")
+        warning(
+            paste0(
+                "'robustness' is a reserved parameter for robustly_optimize. ",
+                "Replacing it from the supplied parameter list."
+            )
+        )
         parameters$robustness<- NULL
     } else {}
     parameters<- c(
-        parameters,
-        list(
-            robustness = robust_schedule[[1]]
-        )
-    )
-    parameters<- utils::as.relistable(parameters)
-    if( "robustness" %in% names(map) ) map$robustness<- NULL
-    robust_map<- list(
-        robustness = as.factor(NA)
-    )
-    spline_map<- list(
-        node_values = as.factor(
-            rep(
-                NA,
-                length(parameters[[smooth]])
+            parameters,
+            list(
+                robustness = robust_schedule[[1]]
             )
-        )
+        ) |> 
+        utils::as.relistable()
+    if( "robustness" %in% names(map) ) map$robustness<- NULL
+    robust_map<- list(robustness = as.factor(NA))
+    spline_map<- list(
+        node_values = NA |> 
+            rep(parameters[[smooth]] |> length()) |>
+            as.factor()
     )
     names(spline_map)<- smooth
     spline_map<- c(
@@ -75,41 +85,37 @@ robustly_optimize<- function(
         spline_map
     )
 
-    parameter_tracing<- lapply(
-        robust_schedule,
-        function(r) {
-            return(
-                list(
-                    robustness = r,
-                    parameters = parameters,
-                    opt = NA
+    parameter_tracing<- robust_schedule |>
+        lapply(
+            function(r) {
+                return(
+                    list(
+                        robustness = r,
+                        parameters = parameters,
+                        opt = NA
+                    )
                 )
-            )
-        }
-    )
+            }
+        )
 
     if( !silent & length(robust_schedule) > 0) cat("Creating taped function.\n")
-    if( length(robust_schedule) == 1) {
-        f<- func
-    } else {
-        f<- RTMB::MakeTape(
-            func,
-            parameters
+    f<- func
+    if( length(robust_schedule) > 1) f<- f |> RTMB::MakeTape(parameters)
+    if( !silent ) cat(
+            paste0(
+                "(", parameters$robustness,
+                " -> ", max(robust_schedule),
+                ") Starting optimization."
+            )
         )
-    }
-    if( !silent ) cat(paste0("(", parameters$robustness, ") Starting optimization.\n"))
-    obj<- RTMB::MakeADFun(
-        f,
-        parameters,
-        map = c(map, robust_map),
-        random = random,
-        silent = TRUE
-    )
-    opt<- stats::nlminb(
-        obj$par,
-        obj$fn,
-        obj$gr
-    )
+    obj<- f |> 
+        RTMB::MakeADFun(
+            parameters,
+            map = c(map, robust_map),
+            random = random,
+            silent = TRUE
+        )
+    opt<- suppressWarnings(with(obj, stats::nlminb(par, fn, gr)))
     fitpar<- obj$env$parList()
     parameter_tracing[[1]]$parameters<- fitpar
     parameter_tracing[[1]]$opt<- opt
@@ -117,59 +123,51 @@ robustly_optimize<- function(
         r<- robust_schedule[i]
         fitpar$robustness<- r
         # 1.) Tighten spline estimate to remove pull from outliers
-        if( !silent ) cat(paste0("\r\033[K(", fitpar$robustness, ") Tightening random effects. "))
-        fitpar[[smooth]]<- tighten(fitpar[[smooth]])
+        if( !silent ) cat(
+                paste0(
+                    "\r\033[K(", fitpar$robustness,
+                    " -> ", max(robust_schedule),
+                    ") Tightening random effects. "
+                )
+            )
+        fitpar[[smooth]]<- fitpar[[smooth]] |> tighten()
 
         # 2.) Re-estimate parameters holding spline fixed
         if( !silent ) cat(paste0("Re-estimating fixed parameters. "))
-        obj<- RTMB::MakeADFun(
-            f,
-            fitpar,
-            map = c(spline_map, robust_map),
-            random = random,
-            silent = TRUE
-        )
-        opt<- stats::nlminb(
-            obj$par,
-            obj$fn,
-            obj$gr
-        )
+        obj<- f |> 
+            RTMB::MakeADFun(
+                fitpar,
+                map = c(spline_map, robust_map),
+                random = random,
+                silent = TRUE
+            )
+        opt<- suppressWarnings(with(obj, stats::nlminb(par, fn, gr)))
         fitpar<- obj$env$parList()
 
         # 3.) Re-estimate spline and parameters
         if( !silent ) cat(paste0("Re-estimating all parameters."))
         if( !silent & (i == length(robust_schedule)) ) cat("\n")
-        obj<- RTMB::MakeADFun(
-            f,
-            fitpar,
-            map = c(map, robust_map),
-            random = random,
-            silent = TRUE
-        )
-        opt<- stats::nlminb(
-            obj$par,
-            obj$fn,
-            obj$gr
-        )
+        obj<- f |>
+            RTMB::MakeADFun(
+                fitpar,
+                map = c(map, robust_map),
+                random = random,
+                silent = TRUE
+            )
+        opt<- suppressWarnings(with(obj, stats::nlminb(par, fn, gr)))
         fitpar<- obj$env$parList()
 
         parameter_tracing[[i]]$parameters<- fitpar
         parameter_tracing[[i]]$opt<- opt
     }
 
-    convergences<- do.call(
-        c,
-        lapply(
-            parameter_tracing,
-            function(x) {
-                return(x$opt$convergence)
-            }
-        )
-    )
-    if( any(convergences == 0) ) {
-        last_converged<- max(which(convergences == 0))
+    convergences<- c |> 
+        do.call(parameter_tracing |> lapply(function(x) x$opt$convergence))
+    if( (convergences == 0) |> any() ) {
+        last_converged<- (convergences == 0) |> which() |> max()
     } else {
-        last_converged<- length(convergences)
+        warning("Robust optimization did not converge.")
+        last_converged<- convergences |> length()
     }
     if( !silent & (last_converged != length(convergences)) ) {
         cat(
@@ -185,21 +183,21 @@ robustly_optimize<- function(
 
     if( !silent ) cat("Re-creating ADFun. ")
     if( length(robust_schedule) > 1 ) {
-        obj<- RTMB::MakeADFun(
-            func,
-            fitpar,
-            map = c(map, robust_map),
-            random = random,
-            silent = TRUE
-        )
+        obj<- func |>
+            RTMB::MakeADFun(
+                fitpar,
+                map = c(map, robust_map),
+                random = random,
+                silent = TRUE
+            )
     } else {}
     
     if( !silent ) cat("Computing sdreport.\n")
-    sdr<- RTMB::sdreport(
-        obj,
-        opt$par,
-        getJointPrecision = TRUE
-    )
+    sdr<- obj |> 
+        RTMB::sdreport(
+            opt$par,
+            getJointPrecision = TRUE
+        )
     
     return(
         list(
--- R/robustifyRTMB.R
+++ R/robustifyRTMB.R
@@ -2,32 +2,44 @@
 #' 
 #' Compute the robustified likelihood value
 #' 
-#' @param x A vector of log-likelihood values
-#' @param robustness A positive number. Larger numbers downweight components more.
-#' @param robust_function Either "id" for the identity (non-robust), "ll" for log-logistic, or "ssh" for smooth semi-huber.
+#' @param x 
+#'     A vector of log-likelihood values
+#' @param robustness 
+#'     A positive number. Larger numbers downweight components more.
+#' @param robust_function 
+#'     Either "id" for the identity (non-robust), "ll" for log-logistic, or 
+#'     "ssh" for smooth semi-huber. "ssh" is currently an experimental function
+#'     and may quietly not work as intended.
 #' 
-#' @return The robustified log-likelihood values.
+#' @return 
+#'     The robustified log-likelihood values.
 #' 
 #' @export
 robustify<- function(x, robustness, robust_function = "id") NULL
 
 #' Compute the robust weight of a log-likelihood value
 #' 
-#' @return A vector of robust weights.
+#' @return 
+#'     A vector of robust weights.
 #' 
-#' @describeIn robustify Compute the robust weight of log-likelihood values
+#' @describeIn robustify 
+#'     Compute the robust weight of log-likelihood values
 #' 
 #' @export
 robust_weight<- function(x, robustness, robust_function = "id") NULL
 
-.onLoad<- function(libname, pkgname) {
+.onLoad<- function(
+        libname, 
+        pkgname
+    ) {
     .orig_TapeConfig<- RTMB::TapeConfig()
     RTMB::TapeConfig(comparison = "tape")
 
     .ll<- RTMB::MakeTape(
         function(y) {
             x<- y[[1]]
-            c<- 1 / (y[[2]] + 0.002) # give upper bound to c to avoid overflow later
+            # Add a small value to give upper bound to c to avoid overflow later
+            c<- 1 / (y[[2]] + 0.002)
             ans<- log(1 + exp(x + c)) - log(1 + exp(c))
             ans<- (y[[2]] == 0) * x + (y[[2]] > 0) * ans
             return(ans)
@@ -50,50 +62,58 @@ robust_weight<- function(x, robustness, robust_function = "id") NULL
     )$atomic()
     .ssh_jac<- .ssh$jacfun()
 
-    do.call(RTMB::TapeConfig, as.list(.orig_TapeConfig))
+    RTMB::TapeConfig |> do.call(as.list(.orig_TapeConfig))
 
-    robustify<- function(x, robustness, robust_function = "id") {
-        ans<- RTMB::sapply(
-            x,
-            function(xx) {
-                yy<- switch(
-                    robust_function,
-                    "id" = xx,
-                    "ll" = .ll(c(xx, robustness)),
-                    "ssh" = .ssh(c(xx, robustness)),
-                    default = xx
-                )
-                return(yy)
-            }
-        )
-        return(ans)
+    robustify<- function(
+            x, 
+            robustness, 
+            robust_function = "id"
+        ) {
+        ans<- x |> 
+            RTMB::sapply(
+                function(xx) {
+                    yy<- switch(
+                        robust_function,
+                        "id" = xx,
+                        "ll" = .ll(c(xx, robustness)),
+                        "ssh" = .ssh(c(xx, robustness)),
+                        default = xx
+                    )
+                    return(yy)
+                }
+            )
+        return( ans )
     }
     assign(
         "robustify",
         robustify,
-        parent.env(environment())
+        environment() |> parent.env()
     )
 
-    robust_weight<- function(x, robustness, robust_function = "id") {
-        ans<- RTMB::sapply(
-            x,
-            function(xx) {
-                yy<- switch(
-                    robust_function,
-                    "id" = 1,
-                    "ll" = .ll_jac(c(xx, robustness))[1],
-                    "ssh" = .ssh_jac(c(xx, robustness))[1],
-                    default = 1
-                )
-                return(yy)
-            }
-        )
-        return(ans)
+    robust_weight<- function(
+            x, 
+            robustness, 
+            robust_function = "id"
+        ) {
+        ans<- x |> 
+            RTMB::sapply(
+                function(xx) {
+                    yy<- switch(
+                        robust_function,
+                        "id" = 1,
+                        "ll" = .ll_jac(c(xx, robustness))[1],
+                        "ssh" = .ssh_jac(c(xx, robustness))[1],
+                        default = 1
+                    )
+                    return(yy)
+                }
+            )
+        return( ans )
     }
     assign(
         "robust_weight",
         robust_weight,
-        parent.env(environment())
+        environment() |> parent.env()
     )
 
     invisible()
